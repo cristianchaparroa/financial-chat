28670ab252d58d0cb1c719e6df770e4902644909
cristianchaparroa <cristianchaparroa@gmail.com>
2020-08-03 23:28:42 -0500

    test: add unit testing for Login manager

---
+++ backend/accounts/managers/login_manager_test.go
@@ -0,0 +1,42 @@
+package managers
+
+import (
+	"chat/accounts/ports"
+	"chat/accounts/ports/mocks"
+	"chat/app/accounts/adapters/repositories/fixtures"
+	"errors"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type loginManagerSuite struct {
+	suite.Suite
+	accManager *mocks.AccountManager
+	token *mocks.TokenManager
+
+	manager ports.LoginManager
+}
+
+func TestLoginManagerSuiteInit(t *testing.T) {
+	suite.Run(t, new(loginManagerSuite))
+}
+
+func (s *loginManagerSuite) SetupSuite() {
+	s.accManager = &mocks.AccountManager{}
+	s.token = &mocks.TokenManager{}
+	s.manager = newLoginManager(s.accManager, s.token)
+}
+
+func (s *loginManagerSuite) TestLoginAccountNotFound() {
+	e := fixtures.GetAccount()
+	acc := e.ToDomain()
+
+	s.accManager.On("Login", mock.Anything).
+		Return(nil, errors.New(userNotFound)).
+		Once()
+
+	_,token,  err := s.manager.Login(acc)
+	s.NotNil(err)
+	s.Empty(token)
+}
